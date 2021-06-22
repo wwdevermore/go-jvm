3b59d9b110bdceac49baa137bc26dabeada64bb4
lauda wong <8093442-weidawang@users.noreply.gitlab.com>
2021-06-23 01:41:53 +0800

    constant pool

---
+++ ch02/classfile/member_info.go
@@ -0,0 +1,40 @@
+package classfile
+
+type MemberInfo struct {
+	cp              ConstantPool
+	accessFlags     uint16
+	nameIndex       uint16 //成员的字段名或方法名索引（常量池）
+	descriptorIndex uint16 //成员的描述符索引（常量池）
+	attributes      []AttributeInfo
+}
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	memberCount := reader.readUint16()
+	members := make([]*MemberInfo, memberCount)
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	return &MemberInfo{
+		cp:              cp,
+		accessFlags:     reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		attributes:      readAttributes(reader, cp),
+	}
+}
+
+func (self *MemberInfo) Accessflags() uint16 {
+	return self.accessFlags
+}
+
+func (self *MemberInfo) Name() string {
+	return self.cp.getUft8(self.nameIndex)
+}
+
+func (self *MemberInfo) Descriptor() string {
+	return self.cp.getUft8(self.descriptorIndex)
+}
