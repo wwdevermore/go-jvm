700f333d367215f023a6a830581d83768aa7b303
lauda wong <8093442-weidawang@users.noreply.gitlab.com>
2021-06-28 00:41:33 +0800

    rtda

---
+++ ch04/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"jvm/ch02/classfile"
+	"jvm/ch02/classpath"
+	"strings"
+)
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Println("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+
+}
+
+func startJVM(cmd *Cmd) {
+	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	fmt.Println(cmd.class)
+	printClassinfo(cf)
+}
+
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		panic(err)
+	}
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+	return cf
+}
+
+func printClassinfo(cf *classfile.ClassFile) {
+	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
+	fmt.Printf("constant count: %v\n", len(cf.ConstantPool()))
+	fmt.Printf("access flags: 0x%X\n", cf.AccessFlags())
+	fmt.Printf("super class: %v\n", cf.SuperClassName())
+	for _, f := range cf.Fields() {
+		fmt.Printf("  %s\n", f.Name())
+	}
+}
