f68c95af7895c5b4278fbe3757faf5fbb355313e
wwd王威达 <weidawang@Ctrip.com>
2021-07-08 20:02:36 +0800

    amend

--- ch05/instructions/base/invoke_method_logic.go
+++ ch05/instructions/base/invoke_method_logic.go
@@ -1,9 +1,19 @@
-package heap
+package base
 
-// import "go-jvm/ch05/rtda"
+import (
+	"go-jvm/ch05/rtda"
+	"go-jvm/ch05/rtda/heap"
+)
 
-// func invokeMethod(invokeFrame *rtda.Frame, method *Method) {
-// 	thread := invokeFrame.Thread()
-// 	newFrame := thread.NewFrame(method)
-// 	newFrame
-// }
+func invokeMethod(invokeFrame *rtda.Frame, method *heap.Method) {
+	thread := invokeFrame.Thread()
+	newFrame := thread.NewFrame(method)
+	thread.PushFrame(newFrame)
+	argSlotCount := int(method.ArgSlotCount())
+	if argSlotCount > 0 {
+		for i := argSlotCount - 1; i >= 0; i-- {
+			slot := invokeFrame.OperandStack().PopSlot()
+			newFrame.LocalVars().SetSlot(uint(i), slot)
+		}
+	}
+}
